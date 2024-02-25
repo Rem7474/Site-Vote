39480167bc3c779641c94757fc11f5a12c55a4db
Rem7474 <recuv007@hotmail.fr>
2024-02-25 11:35:23 +0100

    Mise à jour du formulaire de vote avec des styles CSS et une structure HTML améliorée

--- formulaireVote.php
+++ formulaireVote.php
@@ -23,23 +23,27 @@ else{
     <meta charset="utf-8">
     <title>Vote</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-    <h1>Vote pour le BDE R&T</h1>
-    <p>Bienvenue sur la page de vote pour le BDE R&T. Pour voter, veuillez choisir un candidat dans la liste ci-dessous.</p>
+    <div class="container">
+        <h1>Vote pour le BDE R&T</h1>
+        <p>Bienvenue sur la page de vote pour le BDE R&T. Pour voter, veuillez choisir un candidat dans la liste ci-dessous.</p>
 
-    <form action="index.php" method="post">
-        <div class="gauche">
-            <input id="candidat1" type="radio" name="candidat" value="1"><br>
-            <label for="candidat1">Equipe de ....</label>
-            <img src="candidat1.jpg" alt="Photo de l'équipe de ...."><br>
-        </div>
-        <div class="droite">
-            <input id="candidat2" type="radio" name="candidat" value="2"><br>
-            <label for="candidat2">Equipe de ....</label>
-            <img src="candidat2.jpg" alt="Photo de l'équipe de ...."><br>
-        </div>
-        <input type="hidden" name="hash" value="<?php echo $hash; ?>">
-        <input type="submit" value="Voter">
-    </form>
+        <form action="index.php" method="post" class="vote-form">
+            <div class="candidate">
+                <input id="candidat1" type="radio" name="candidat" value="1">
+                <label for="candidat1">Equipe de ....</label>
+                <img src="candidat1.jpg" alt="Photo de l'équipe de ....">
+            </div>
+            <div class="candidate">
+                <input id="candidat2" type="radio" name="candidat" value="2">
+                <label for="candidat2">Equipe de ....</label>
+                <img src="candidat2.jpg" alt="Photo de l'équipe de ....">
+            </div>
+            <input type="hidden" name="hash" value="<?php echo $hash; ?>">
+            <input type="submit" value="Voter">
+        </form>
+    </div>
+</body>
 </html>
